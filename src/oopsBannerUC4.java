a3b5649a288fea24d3591525a6672520c0648909
ajay-200717 <ak7311@srmist.edu.in>
2026-03-10 09:33:37 +0530

    Initial project

---
+++ src/oopsBannerUC4.java
@@ -0,0 +1,19 @@
+public class oopsBannerUC4 {
+    public static void main(String[] args){
+        String[] banner = {
+        "   ***           ***        ******            ******  ",
+        " **   **       **   **      **    **        **        ",
+        "**     **     **     **     **     **      **         ",
+        "**     **     **     **     **    **        **        ",
+        "**     **     **     **     ******            ***     ",
+        "**     **     **     **     **                  **   ",
+        "**     **     **     **     **                    **  ",
+        " **   **       **   **      **                    **  ",
+        "   ***           ***        **              *****     "
+    };
+        for(String line : banner){
+            System.out.println(line);
+        }
+    }
+} 
+
