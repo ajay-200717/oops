4733d753a160c0d5f0c9c3845043ba7242496e60
ajay-200717 <ak7311@srmist.edu.in>
2026-03-10 10:03:49 +0530

    Initial project

---
+++ src/ArrayList.java
@@ -0,0 +1,23 @@
+public class ArrayList {
+    public static void main(String[] args) {
+        System.out.println("OOPS Banner App - UC5");
+
+        // Inline array initialization with String.join()
+        String[] bannerLines = {
+            String.join("", "   ***      ", "     ***     ", "  ******      ", "   ******   "),
+            String.join("", " **   **    ", "   **   **   ", "  **    **    ", " **         "),
+            String.join("", "**     **   ", "  **     **  ", "  **     **   ", " **         "),
+            String.join("", "**     **   ", "  **     **  ", "  **    **    ", "   **       "),
+            String.join("", "**     **   ", "  **     **  ", "  ******      ", "     ***    "),
+            String.join("", "**     **   ", "  **     **  ", "  **          ", "        **  "),
+            String.join("", "**     **   ", "  **     **  ", "  **          ", "         ** "),
+            String.join("", " **   **    ", "   **   **   ", "  **          ", "        **  "),
+            String.join("", "   ***      ", "     ***     ", "  **          ", "   ******   ")
+        };
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
+    
+
